d2aef06d29cf84b57046f3f2fac74c4047cb40df
Gabriel Raulet <gabe.h.raulet@gmail.com>
2024-07-10 18:11:19 -0700

    graph_inserter

--- include/ptidx.hpp
+++ include/ptidx.hpp
@@ -1,23 +1,38 @@
 template <class PointTraits, class Index>
+struct GraphInserter
+{
+    using Real = typename PointTraits::Real;
+    using Point = typename PointTraits::Point;
+    using PointVector = vector<Point>;
+
+    template <class NeighborContainer>
+    Index operator()(const PointVector& points, Real radius, vector<NeighborContainer>& graph) const
+    {
+        graph.clear();
+        auto distance = PointTraits::distance();
+        Index num_edges = 0;
+
+        for (Index i = 0; i < points.size(); ++i)
+        {
+            graph.emplace_back();
+            auto& neighs = graph.back();
+            auto it = inserter(neighs, neighs.end());
+
+            for (Index j = 0; j < points.size(); ++j)
+                if (distance(points[i], points[j]) <= radius)
+                    *it++ = j;
+
+            num_edges += neighs.size();
+        }
+
+        return num_edges;
+    }
+};
+
+template <class PointTraits, class Index>
 Index BruteForcer<PointTraits, Index>::build_rgraph(Real radius, IndexSetVector& graph) const
 {
-    graph.clear();
-    auto distance = PointTraits::distance();
-    Index num_edges = 0;
-
-    for (Index i = 0; i < size(); ++i)
-    {
-        graph.emplace_back();
-        auto& neighs = graph.back();
-
-        for (Index j = 0; j < size(); ++j)
-            if (distance(points[i], points[j]) <= radius)
-                neighs.insert(j);
-
-        num_edges += neighs.size();
-    }
-
-    return num_edges;
+    return GraphInserter<PointTraits, Index>()(points, radius, graph);
 }
 
 template <class PointTraits, class Index>
@@ -25,19 +40,7 @@ template <class Iter> requires is_iter_type<Iter, typename PointTraits::Point>
 void PrunedForcer<PointTraits, Index>::assign(Iter first, Iter last)
 {
     points.assign(first, last);
-    cutoff_neighs.clear();
-
-    auto distance = PointTraits::distance();
-
-    for (Index i = 0; i < size(); ++i)
-    {
-        cutoff_neighs.emplace_back();
-        auto& es = cutoff_neighs.back();
-
-        for (Index j = 0; j < size(); ++j)
-            if (distance(points[i], points[j]) <= cutoff)
-                es.push_back(j);
-    }
+    GraphInserter<PointTraits, Index>()(points, cutoff, cutoff_neighs);
 }
 
 template <class PointTraits, class Index>
