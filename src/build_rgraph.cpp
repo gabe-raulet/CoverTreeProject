589f363f72f19ef9bde2598a9ab19a3b70006a0a
Gabriel Raulet <gabe.h.raulet@gmail.com>
2024-07-11 12:56:43 -0700

    add base option

--- src/build_rgraph.cpp
+++ src/build_rgraph.cpp
@@ -21,12 +21,15 @@ using Point = PointTraits::Point;
 
 enum Indexer {BruteOpt, TreeOpt};
 
-void read_options(int argc, char *argv[], char *&infname, char *&outfname, double& radius, Indexer& indexer);
+void read_options(int argc, char *argv[], char *&infname, char *&outfname, double& radius, double& base, Indexer& indexer);
 void read_points_file(vector<Point>& points, const char *fname);
+
+template <class Kind>
+void build_point_index(PointIndex<Kind>& ptidx, const vector<Point>& points);
 
 int main(int argc, char *argv[])
 {
-    double radius;
+    double radius, base = 2.0;
     char *infname, *outfname;
     Indexer indexer = TreeOpt;
 
@@ -35,7 +38,7 @@ int main(int argc, char *argv[])
 
     vector<Point> points;
 
-    read_options(argc, argv, infname, outfname, radius, indexer);
+    read_options(argc, argv, infname, outfname, radius, base, indexer);
     read_points_file(points, infname);
 
     timer.stop_timer();
@@ -44,25 +47,27 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void read_options(int argc, char *argv[], char *&infname, char *&outfname, double& radius, Indexer& indexer)
+void read_options(int argc, char *argv[], char *&infname, char *&outfname, double& radius, double& base, Indexer& indexer)
 {
     LocalTimer timer;
     timer.start_timer();
 
-    auto usage = [&argv, &indexer](int err)
+    auto usage = [&argv, &indexer, &base](int err)
     {
         fprintf(stderr, "Usage: %s [options] <ptsfname> <graphfname> <radius>\n", argv[0]);
         fprintf(stderr, "Options: -T  use cover tree indexer [default]\n");
-        fprintf(stderr, "         -B  use brute force indexer\n");
+        fprintf(stderr, "         -F  use brute force indexer\n");
+        fprintf(stderr, "         -b  cover tree base [%.2f]\n", base);
         fprintf(stderr, "         -h  help message\n");
         exit(err);
     };
 
     int c;
-    while ((c = getopt(argc, argv, "TBh")) >= 0)
+    while ((c = getopt(argc, argv, "TFb:h")) >= 0)
     {
         if      (c == 'T') indexer = TreeOpt;
-        else if (c == 'B') indexer = BruteOpt;
+        else if (c == 'F') indexer = BruteOpt;
+        else if (c == 'b') base = atof(optarg);
         else if (c == 'h') usage(0);
     }
 
@@ -92,4 +97,17 @@ void read_points_file(vector<Point>& points, const char *fname)
     timer.stop_timer();
 
     fprintf(stderr, "[time=%.3f,msg::%s] read points file '%s' [numpts=%lu,filesize=%s]\n", timer.get_elapsed(), __func__, fname, static_cast<size_t>(points.size()), PrettyFileSize::str(fname).c_str());
+}
+
+template <class Kind>
+void build_point_index(PointIndex<Kind>& ptidx, const vector<Point>& points)
+{
+    LocalTimer timer;
+    timer.start_timer();
+
+    ptidx.build(points.begin(), points.end());
+
+    timer.stop_timer();
+
+    fprintf(stderr, "[time=%.3f,msg::%s] :: built index using %s indexer\n", timer.get_elapsed(), __func__, ptidx.repr());
 }
